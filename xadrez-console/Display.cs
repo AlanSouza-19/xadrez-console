d426b306bc083f2281c18ce60e4977b0ee4ec0e4
Alan Corrêa <souzaalanc75@gmail.com>
2024-06-06 19:55:49 -0300

    melhorando implementação do método imprimir tabuleiro

--- xadrez-console/Display.cs
+++ xadrez-console/Display.cs
@@ -6,16 +6,32 @@ public class Display
     public static void PrintBoard(Board board) {
         for (int i=0; i < board.Rows; i++)
         {
+            Console.Write($"{8-i} ");
             for (int j=0; j < board.Columns; j++)
             {
                 if (board.Piece(i, j) == null)
                 {
                     Console.Write("- ");
                 } else {
-                    Console.Write(board.Piece(i, j) + " ");
+                    PrintPiece(board.Piece(i, j));
+                    Console.Write(" ");
                 }
             }
             Console.WriteLine();
+        }
+        Console.WriteLine("  a b c d e f g h");
+    }
+
+    public static void PrintPiece(Piece p)
+    {
+        if (p.Color == Color.White)
+        {
+            Console.Write(p);
+        } else {
+            ConsoleColor aux = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(p);
+            Console.ForegroundColor = aux;
         }
     }
 }
--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -4,11 +4,14 @@ using xadrez_console.chess;
 namespace xadrez_console;
 class Program {
     public static void Main(string[] args) {
-        Board bd = new (8, 8);
+        Board bd = new Board(8, 8);
         try
         {
-            ChessPosition cp = new ChessPosition('c', 7);
-            Console.WriteLine(cp.ToPosition());
+            bd.PutPiece(new Rook(bd, Color.White), new Position(0, 0));
+            bd.PutPiece(new Rook(bd, Color.White), new Position(0, 7));
+            bd.PutPiece(new Rook(bd, Color.Black), new Position(7, 0));
+            bd.PutPiece(new Rook(bd, Color.Black), new Position(7, 7));
+            Display.PrintBoard(bd);
         } catch (BoardException e)
         {
             Console.WriteLine(e.Message);
