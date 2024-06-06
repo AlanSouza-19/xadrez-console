4bcfa4fedf302aa803d35aa175735395c3fc487d
Alan Corrêa <souzaalanc75@gmail.com>
2024-06-06 19:39:37 -0300

    criando a classe posição xadrez

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -7,11 +7,8 @@ class Program {
         Board bd = new (8, 8);
         try
         {
-            bd.PutPiece(new Rook(bd, Color.Black), new Position(0, 8));
-            bd.PutPiece(new Rook(bd, Color.Black), new Position(0, 0));
-            bd.PutPiece(new Rook(bd, Color.Black), new Position(0, 7));
-            Display.PrintBoard(bd);
-            Console.ReadLine();
+            ChessPosition cp = new ChessPosition('c', 7);
+            Console.WriteLine(cp.ToPosition());
         } catch (BoardException e)
         {
             Console.WriteLine(e.Message);
---
+++ xadrez-console/chess/ChessPosition.cs
@@ -0,0 +1,18 @@
+﻿using xadrez_console.board;
+
+namespace xadrez_console.chess;
+public class ChessPosition(char column, int row)
+{
+    public char Column { get; set; } = column;
+    public int Row { get; set; } = row;
+
+    public override string ToString()
+    {
+        return $"{Column}{Row}";
+    }
+
+    public Position ToPosition()
+    {
+        return new Position(8 - Row, Column - 'a');
+    }
+}
